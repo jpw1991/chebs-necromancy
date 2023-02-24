ea728d8161bdb6eb5057d996bdb4f8769067e811
Dracbjorn <Kentgrav@gmail.com>
2023-02-24 18:08:55 -0500

    Commiting progress

--- ChebsNecromancy/Common/ChebsPrefab.cs
+++ ChebsNecromancy/Common/ChebsPrefab.cs
@@ -1,43 +1,26 @@
-﻿using BepInEx.Configuration;
-using ChebsNecromancy.Items;
-using Jotunn.Configs;
+﻿using Jotunn.Configs;
 using Jotunn.Entities;
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static Piece;
 using UnityEngine;
 using Logger = Jotunn.Logger;
 
 namespace ChebsNecromancy.Common
 {
-    public class ChebsRecipe
+    internal class ChebsPrefab
     {
-        public string RecipeValue = "<Prefab1>:<quantity>[[,<PreFab2>:<quantity>], ...]";
-        public ConfigEntry<bool> Allowed { get; set; }
-        public ConfigEntry<string> CraftingCost { get; set; }
-        public ConfigEntry<EcraftingTable> CraftingStationRequired { get; set; }
-        public ConfigEntry<int> CraftingStationLevel { get; set; }
-
-        public enum EcraftingTable
-        {
-            None,
-            [InternalName("piece_workbench")] Workbench,
-            [InternalName("piece_cauldron")] Cauldron,
-            [InternalName("forge")] Forge,
-            [InternalName("piece_artisanstation")] ArtisanTable,
-            [InternalName("piece_stonecutter")] StoneCutter
-        }
-
-        public string DefaultRecipe { get; set; }
+        public bool Allowed { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeDescription { get; set; }
         public string PieceTable { get; set; }
         public string PieceCategory { get; set; }
-        public string RecipeName { get; set; }
-        public string ItemName { get; set; }
-        public string RecipeDescription { get; set; }
+        public string DefaultRecipe { get; set; }
+        public string CraftingCost { get; set; }
         public string PrefabName { get; set; }
-        public string IconName { get; set; }
-        public string ObjectName { get; set; }
-        public EcraftingTable CraftingTable { get; set; }
 
         public CustomPiece GetCustomPieceFromPrefab<T>(GameObject prefab, Sprite icon = null)
         {
@@ -75,7 +58,7 @@ namespace ChebsNecromancy.Common
             {
                 return (T)Convert.ChangeType(null, typeof(T));
             }
-            
+
             if (Allowed.Value)
             {
                 if (string.IsNullOrEmpty(CraftingCost.Value))
@@ -137,55 +120,6 @@ namespace ChebsNecromancy.Common
             else
             {
                 return (T)Convert.ChangeType(null, typeof(T));
-            }
-        }
-
-        private void SetRecipeReqs<T>(
-            T config,
-            ConfigEntry<string> craftingCost,
-            ConfigEntry<EcraftingTable> craftingStationRequired = null,
-            ConfigEntry<int> craftingStationLevel = null
-            )
-        {
-            // function to add a single material to the recipe
-            void addMaterial(string material)
-            {
-                string[] materialSplit = material.Split(':');
-                string materialName = materialSplit[0];
-                int materialAmount = int.Parse(materialSplit[1]);
-                if (config.GetType() == typeof(ItemConfig))
-                {
-                    (config as ItemConfig).AddRequirement(new RequirementConfig(materialName, materialAmount, 0, true));
-                }
-                else if (config.GetType() == typeof(PieceConfig))
-                {
-                    (config as PieceConfig).AddRequirement(new RequirementConfig(materialName, materialAmount, 0, true));
-                }
-            }
-
-            // Settings for item configs only
-            if (craftingStationRequired is not null)
-            {
-                (config as ItemConfig).CraftingStation = ((InternalName)typeof(EcraftingTable).GetMember(
-                    craftingStationRequired.Value.ToString())[0].GetCustomAttributes(
-                    typeof(InternalName)).First()).Name;
-
-                (config as ItemConfig).MinStationLevel = (craftingStationLevel is null ? 1 : craftingStationLevel.Value);
-            }
-
-            // build the recipe. material config format ex: Wood:5,Stone:1,Resin:1
-            if (craftingCost.Value.Contains(','))
-            {
-                string[] materialList = craftingCost.Value.Split(',');
-
-                foreach (string material in materialList)
-                {
-                    addMaterial(material);
-                }
-            }
-            else
-            {
-                addMaterial(craftingCost.Value);
             }
         }
     }
