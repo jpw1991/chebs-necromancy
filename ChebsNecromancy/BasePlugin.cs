c7be8c3da483bfb1c4c821d0a1e679520d7fecfa
Joshua Paul Woods <jpw1991@gmail.com>
2023-03-08 12:24:39 -0800

    2.3.2: hitting minions with a skeleton wand kills them

--- ChebsNecromancy/BasePlugin.cs
+++ ChebsNecromancy/BasePlugin.cs
@@ -31,7 +31,7 @@ namespace ChebsNecromancy
     {
         public const string PluginGuid = "com.chebgonaz.ChebsNecromancy";
         public const string PluginName = "ChebsNecromancy";
-        public const string PluginVersion = "2.3.1";
+        public const string PluginVersion = "2.3.2";
         private const string ConfigFileName =  PluginGuid + ".cfg";
         private static readonly string ConfigFileFullPath = Path.Combine(Paths.ConfigPath, ConfigFileName);
 
