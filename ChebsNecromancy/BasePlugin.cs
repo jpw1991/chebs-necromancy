f48b95fc4b9d697f2b71fde3eaf6eae578454d80
Joshua Paul Woods <jpw1991@gmail.com>
2023-04-17 17:52:24 -0700

    translations

--- ChebsNecromancy/BasePlugin.cs
+++ ChebsNecromancy/BasePlugin.cs
@@ -38,7 +38,7 @@ namespace ChebsNecromancy
         private const string ConfigFileName =  PluginGuid + ".cfg";
         private static readonly string ConfigFileFullPath = Path.Combine(Paths.ConfigPath, ConfigFileName);
         
-        public readonly System.Version ChebsValheimLibraryVersion = new("1.1.1");
+        public readonly System.Version ChebsValheimLibraryVersion = new("1.1.2");
 
         private readonly Harmony harmony = new(PluginGuid);
 
--- localization_string_finder.py
+++ localization_string_finder.py
@@ -7,7 +7,7 @@ for root, dirs, files in os.walk("ChebsNecromancy"):
     path = root.split(os.sep)
     for file in files:
         if file.endswith(".cs"):
-            with open(os.sep.join(path+[file]), 'r') as f:
+            with open(os.sep.join(path+[file]), 'r', encoding='utf-8', errors='ignore') as f:
                 for line in f.readlines():
                     x = re.findall('''"\$[A-Za-z_]+"''', line)
                     if x and len(x) > 0:
