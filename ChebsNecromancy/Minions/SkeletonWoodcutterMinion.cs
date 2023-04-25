be42a05e8946eb1816beb2e5e2cf407d67881a05
Joshua Paul Woods <jpw1991@gmail.com>
2023-04-25 09:00:33 -0700

    3.2.0: if a woodcutter is swinging, but missing, the damage gets dealt anyway; remove tooltier stuff for simplicity and streamlining. People can use 3rd party item-alteration mods instead

--- ChebsNecromancy/Minions/SkeletonWoodcutterMinion.cs
+++ ChebsNecromancy/Minions/SkeletonWoodcutterMinion.cs
@@ -6,14 +6,10 @@ namespace ChebsNecromancy.Minions
 {
     internal class SkeletonWoodcutterMinion : SkeletonMinion
     {
-        public static ConfigEntry<int> ToolTier;
         public static ConfigEntry<float> UpdateDelay, LookRadius;
 
         public new static void CreateConfigs(BaseUnityPlugin plugin)
         {
-            ToolTier = plugin.Config.Bind("SkeletonWoodcutter (Server Synced)", "ToolTier",
-                3, new ConfigDescription("The tier of the skeleton's tool: 0 (stone), 1 (flint), 2 (bronze), 3 (iron) or 4 (black metal).", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true }));
             UpdateDelay = plugin.Config.Bind("SkeletonWoodcutter (Server Synced)", "SkeletonWoodcutterUpdateDelay",
                 6f, new ConfigDescription("The delay, in seconds, between wood searching attempts. Attention: small values may impact performance.", null,
                     new ConfigurationManagerAttributes { IsAdminOnly = true }));
