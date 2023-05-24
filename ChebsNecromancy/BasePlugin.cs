405b8aac11d19d7cd5ae2250472bd441b7371865
Joshua Paul Woods <jpw1991@gmail.com>
2023-05-24 07:45:15 -0700

    3.3.2: Functions created in 3.3.0 moved into Cheb's Valheim Library for Cheb's Mercenaries

--- ChebsNecromancy/BasePlugin.cs
+++ ChebsNecromancy/BasePlugin.cs
@@ -38,7 +38,7 @@ namespace ChebsNecromancy
     {
         public const string PluginGuid = "com.chebgonaz.ChebsNecromancy";
         public const string PluginName = "ChebsNecromancy";
-        public const string PluginVersion = "3.3.1";
+        public const string PluginVersion = "3.3.2";
         private const string ConfigFileName =  PluginGuid + ".cfg";
         private static readonly string ConfigFileFullPath = Path.Combine(Paths.ConfigPath, ConfigFileName);
         
